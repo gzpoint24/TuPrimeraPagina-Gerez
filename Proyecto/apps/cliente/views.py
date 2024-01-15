7025dcd46b88e5a9c28bf544bf164beab1553b29
Javier Gerez <tienda.gzpoint@gmail.com>
2024-01-15 17:07:31 -0300

    Fin_Modelo_Cliente

--- Proyecto/apps/cliente/views.py
+++ Proyecto/apps/cliente/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from . import models
+from . import forms
+
+def index(request):
+    return render(request, "cliente/index.html")
+
+def cliente_list(request):
+    consulta = models.Cliente.objects.all()
+    contexto = {"listado_clientes": consulta}
+    return render(request, "cliente/cliente_list.html", contexto) 
+
+
+def cliente_create(request):
+    if request.method == "POST":
+        form = forms.ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("cliente:cliente_list")
+    
+    else: 
+        form = forms.ClienteForm()
+    return render(request, "cliente/cliente_create.html", {"form": form})
+
