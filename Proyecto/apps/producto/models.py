9d640ec957698463a21c410d018f9c028cc584ba
Javier Gerez <tienda.gzpoint@gmail.com>
2024-01-16 14:10:02 -0300

    Proyecto Terminado

--- Proyecto/apps/producto/models.py
+++ Proyecto/apps/producto/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Producto(models.Model):
     numero_articulo = models.IntegerField(unique=True)
-    nombre_producto = models.CharField(max_length=100)
+    nombre_producto = models.CharField(max_length=100, unique=True)
     precio = models.DecimalField(max_digits=12, decimal_places=2)
    
     def __str__(self) -> str:
